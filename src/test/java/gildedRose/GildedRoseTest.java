adf686d2fbf3b6cf0736e9f7f2cc31dfd3667795
曹桂凤 <demi.cao@oocl.com>
2019-08-19 20:21:27 +0800

    [Demi][feat:implement test7 and pass it and fic test6 and pass it]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 
 public class GildedRoseTest {
     @Test
-    public void should_return_quality_is_0_when_item_name_not_equals_AgedBrie_but_not_equals_Backstage_and_Sulfuras_and_quality_bigger_than_0() {
+    public void should_return_quality_is_0_when_item_name_not_equals_AgedBrie_and_Backstage_and_Sulfuras_and_quality_bigger_than_0() {
         Item[] items = {new Item("Demi",1,1)};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
@@ -46,10 +46,18 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_quality_is_2_when_sellIn_smaller_than_0_item_name_equals_AgedBrie_and_quality_smaller_than_50() {
-        Item[] items = {new Item("Aged Brie",1,1)};
+    public void should_return_quality_is_3_when_sellIn_smaller_than_0_item_name_equals_AgedBrie_and_quality_smaller_than_50() {
+        Item[] items = {new Item("Aged Brie",-1,1)};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
-        Assert.assertEquals(2,items[0].getQuality());
+        Assert.assertEquals(3,items[0].getQuality());
+    }
+
+    @Test
+    public void should_return_quality_is_4_when_sellIn_smaller_than_0_item_name_not_equals_AgedBrie_but_equals_Backstage() {
+        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",-1,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(0,items[0].getQuality());
     }
 }
