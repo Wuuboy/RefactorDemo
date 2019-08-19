491f19f52b30bf44b764568759eab1e5b5c2c33c
曹桂凤 <demi.cao@oocl.com>
2019-08-20 00:06:27 +0800

    [Demi][feat:add some tests]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -13,8 +13,25 @@ public class GildedRoseTest {
         gildedRose.updateQuality();
         Assert.assertEquals(0,items[0].getQuality());
     }
+
     @Test
-    public void should_return_quality_2_is_when_item_name_equals_AgedBrie_but_not_equals_Backstage_and_Sulfuras_and_quality_smaller_than_50() {
+    public void should_return_quality_is_0_when_item_name_not_equals_AgedBrie_and_Backstage_and_Sulfuras_and_quality_bigger_is_0() {
+        Item[] items = {new Item("Demi",1,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(0,items[0].getQuality());
+    }
+
+    @Test
+    public void should_return_quality_is_0_when_item_name_not_equals_AgedBrie_and_Backstage_and_Sulfuras_and_quality_smaller_is_0() {
+        Item[] items = {new Item("Demi",1,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(0,items[0].getQuality());
+    }
+
+    @Test
+    public void should_return_quality_2_is_when_item_name_equals_AgedBrie_and_quality_smaller_than_50() {
         Item[] items = {new Item("Aged Brie",1,1)};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
@@ -22,7 +39,22 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_quality_3_is_when_item_name_equals_Backstage_but_not_equals_Sulfuras_and_AgedBrie_and_quality_smaller_than_50_and_sellIn_smaller_than_11_bigger_than_6() {
+    public void should_return_quality_2_is_when_item_name_equals_AgedBrie_and_quality_equals_50() {
+        Item[] items = {new Item("Aged Brie",1,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(2,items[0].getQuality());
+    }
+    @Test
+    public void should_return_quality_2_is_when_item_name_equals_AgedBrie_and_quality_bigger_than_50() {
+        Item[] items = {new Item("Aged Brie",1,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(2,items[0].getQuality());
+    }
+
+    @Test
+    public void should_return_quality_3_is_when_item_name_equals_Backstage_and_quality_smaller_than_50_and_sellIn_smaller_than_11_bigger_than_6() {
         Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",7,1)};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
@@ -30,7 +62,39 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_quality_is_4_when_item_name_equals_Backstage_but_not_equals_Sulfuras_and_AgedBrie_and_quality_smaller_than_50_and_sellIn_smaller_than_6() {
+    public void should_return_quality_3_is_when_item_name_equals_Backstage_and_quality_equals_than_50_and_sellIn_smaller_than_6() {
+        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",7,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(3,items[0].getQuality());
+    }
+
+    @Test
+    public void should_return_quality_3_is_when_item_name_equals_Backstage_and_quality_equals_50_and_sellIn_bigger_than_11() {
+        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",7,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(3,items[0].getQuality());
+    }
+
+    @Test
+    public void should_return_quality_3_is_when_item_name_equals_Backstage_and_quality_bigger_than_50_and_sellIn_smaller_than_6() {
+        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",7,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(3,items[0].getQuality());
+    }
+
+    @Test
+    public void should_return_quality_3_is_when_item_name_equals_Backstage_and_quality_bigger_50_and_sellIn_bigger_than_11() {
+        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",7,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(3,items[0].getQuality());
+    }
+
+    @Test
+    public void should_return_quality_is_4_when_item_name_equals_Backstage_and_quality_smaller_than_50_and_sellIn_smaller_than_6() {
         Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",5,1)};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
