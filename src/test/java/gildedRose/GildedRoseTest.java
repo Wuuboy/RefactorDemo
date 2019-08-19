57f4e103d4a679a05f35e609d0f754f1d963f0d4
曹桂凤 <demi.cao@oocl.com>
2019-08-19 20:02:18 +0800

    [Demi][feat:implement test4 and pass it]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -22,10 +22,18 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_quality_is_when_item_name_equals_Backstage_but_not_equals_Sulfuras_and_AgedBrie_and_quality_smaller_than_50_and_sellIn_smaller_than_11_gigger_than_6() {
+    public void should_return_quality_is_when_item_name_equals_Backstage_but_not_equals_Sulfuras_and_AgedBrie_and_quality_smaller_than_50_and_sellIn_smaller_than_11_bigger_than_6() {
         Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",7,1)};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         Assert.assertEquals(3,items[0].getQuality());
+    }
+
+    @Test
+    public void should_return_quality_is_when_item_name_equals_Backstage_but_not_equals_Sulfuras_and_AgedBrie_and_quality_smaller_than_50_and_sellIn_smaller_than_6() {
+        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",5,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(4,items[0].getQuality());
     }
 }
