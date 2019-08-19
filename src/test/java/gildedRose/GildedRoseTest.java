eb500cfde363d9dd8f6396cbed19f3a7f3efc2fc
曹桂凤 <demi.cao@oocl.com>
2019-08-19 19:55:31 +0800

    [Demi][feat:implement test2 and pass it]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -7,11 +7,17 @@ import static org.junit.Assert.*;
 
 public class GildedRoseTest {
     @Test
-    public void should_return_quality_is_when_item_name_equals_AgedBrie_but_not_equals_Backstage_and_Sulfuras_and_quality_bigger_than_0() {
+    public void should_return_quality_is_when_item_name_not_equals_AgedBrie_but_not_equals_Backstage_and_Sulfuras_and_quality_bigger_than_0() {
         Item[] items = {new Item("Demi",1,1)};
         GildedRose gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
         Assert.assertEquals(0,items[0].getQuality());
     }
-
+    @Test
+    public void should_return_quality_is_when_item_name_equals_AgedBrie_but_not_equals_Backstage_and_Sulfuras_and_quality_smaller_than_50() {
+        Item[] items = {new Item("Aged Brie",1,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(2,items[0].getQuality());
+    }
 }
