82ce26f7210b477392191c54332178f9d0ca53f7
曹桂凤 <demi.cao@oocl.com>
2019-08-19 19:50:35 +0800

    [Demi][feat:implement test1 should_return_quality_is_when_item_name_equals_AgedBrie_but_not_equals_Backstage_and_Sulfuras_and_quality_bigger_than_0]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -1,7 +1,17 @@
 package gildedRose;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GildedRoseTest {
+    @Test
+    public void should_return_quality_is_when_item_name_equals_AgedBrie_but_not_equals_Backstage_and_Sulfuras_and_quality_bigger_than_0() {
+        Item[] items = {new Item("Demi",1,1)};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assert.assertEquals(0,items[0].getQuality());
+    }
 
 }
