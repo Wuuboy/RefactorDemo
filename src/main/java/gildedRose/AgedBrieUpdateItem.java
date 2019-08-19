a47be545ce15b5dba46977b1c2ae6a721d9dd863
曹桂凤 <demi.cao@oocl.com>
2019-08-19 23:29:09 +0800

    [Demi][feat:implement AgedBrieUpdateItemc class]

--- src/main/java/gildedRose/AgedBrieUpdateItem.java
+++ src/main/java/gildedRose/AgedBrieUpdateItem.java
@@ -3,6 +3,11 @@ package gildedRose;
 public class AgedBrieUpdateItem extends UpdateItem {
     @Override
     public void updateQuality(Item item) {
+        if (item.isItemQqualitySmallerThan50(item.quality))
+            item.quality += 1;
+        item.decrease1ItemSellIn(item.sellIn);
 
+        if (item.isItemSellInSmallerThan0(item.sellIn))
+            item.quality += 1;
     }
 }
